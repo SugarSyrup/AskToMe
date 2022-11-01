7bb327f4223b3a2cb9115fd9cb97c72ba59e5ebb
JaYun <tlfvm04@naver.com>
2022-11-02 01:39:32 +0900

    준비 : firebase db set

--- src/fbInstace.js
+++ src/fbInstace.js
@@ -1,35 +1,8 @@
-// // import { initializeApp } from "firebase/app";
-// // import { getAnalytics } from "firebase/analytics";
-// // import { getAuth } from "firebase/auth";
-
-
-// // const firebaseConfig = {
-// //     apiKey: process.env.REACT_APP_API_KEY,
-// //     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-// //     projectId: process.env.REACT_APP_PROJECT_ID,
-// //     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-// //     messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
-// //     appId: process.env.REACT_APP_APP_ID
-// // };
-
-// // // const app = initializeApp(firebaseConfig);
-// // // const db = getFirestore(app);
-// // // export const authService = getAuth();
-
-// // const app = initializeApp(firebaseConfig);
-// // const analytics = getAnalytics(app);
-// // // const auth = getAuth();
-
-// // console.log(app);
-
-// // export default app;
-// // // export const authService = firebase.auth()
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
+import "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {

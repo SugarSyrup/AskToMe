373fe61a4f51d53be09442f7ad20d5f5c73cf6b1
JaYun <tlfvm04@naver.com>
2022-11-02 01:38:06 +0900

    Create : LogOut 기능, Nav-bar

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,14 +2,21 @@ import React from "react";
 import {HashRouter as Router, Route, Routes} from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile"
+import Navigation from "components/Navigation";
 
 const AppRouter = (props) => {
     const { isLoggedIn } = props;
+
     return (
         <Router>
+            {isLoggedIn && <Navigation />}
             <Routes>
-                { isLoggedIn ? 
-                <Route exact path="/" element={ <Home /> } /> : <Route exact path="/" element={ <Auth /> } />
+                { isLoggedIn ? <>
+                    <Route exact path="/" element={ <Home /> } />
+                    <Route exact path="/profile" element={ <Profile /> } />
+                </>
+                : <Route exact path="/" element={ <Auth /> } />
                 }      
             </Routes>
         </Router>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { authService } from "fbInstace";
 
-export default () => <span>Profile</span>;
+const Profile = () => {
+    const navigate = useNavigate();
+    const onLogOutClick = () => {
+        authService.signOut();
+        navigate("/");
+    }
+
+    return (<>
+        <button onClick={onLogOutClick}>LOG OUT</button>
+    </>);
+}
+
+export default Profile;

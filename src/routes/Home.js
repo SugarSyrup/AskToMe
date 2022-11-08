922f78bc09dc1474df29f34587cccf1a26a231ff
JaYun <tlfvm04@naver.com>
2022-11-09 00:56:33 +0900

    display Nweets Data

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,24 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 import { dbService } from "fbInstace";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async() => {
+        const dbNweets = await getDocs(collection(dbService, "nweets"));
+        dbNweets.forEach((document) => {
+            const nweetObject = {
+                ...document.data(),
+                id: document.id
+            }
+            setNweets(prev => [nweetObject, ...prev]);
+        });
+    }
+    useEffect(() => {
+        getNweets();
+    }, [])
     const onSubmit = async (event) => {
         event.preventDefault();
         try {
@@ -29,6 +43,13 @@ const Home = () => {
                 <input type="text" placeholder="What's on your mind?" maxLength={20} onChange={onChange} value={nweet}/>
                 <input type="submit" value="Nweet" />
             </form>
+            {
+                nweets.map((nweet) => (
+                    <div key={nweet.id}>
+                        <h4>{nweet.nweet}</h4>
+                    </div>
+                ))
+            }
         </div>
     );
 }

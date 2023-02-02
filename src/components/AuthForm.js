9858c393820ddba215a0bbe4724a219df606b097
SugarSyrup <tlfvm04@naver.com>
2023-02-03 00:34:23 +0900

    setting login/join page

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -5,7 +5,8 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "fire
 const AuthForm = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [newAccount, setNewAccount] = useState(true);
+    const [passwordCheck, setPasswordCheck] = useState("");
+    const [newAccount, setNewAccount] = useState(false);
     const [errorMsg, setErrorMsg] = useState("");
     const onChange = (event) => {
         const {target : {name, value}} = event;
@@ -15,10 +16,17 @@ const AuthForm = () => {
         if(name === "password") {
             setPassword(value);
         }
+        if(name === "passwordCheck") {
+            setPasswordCheck(value);
+        }
     }
 
     const onSubmit = async (event) => {
         event.preventDefault();
+        if(newAccount && password !== passwordCheck) {
+            window.alert("비밀번호가 일치 하지 않습니다.")
+            return;
+        }
         try {
             let data;
             if(newAccount) {
@@ -39,6 +47,7 @@ const AuthForm = () => {
         <form onSubmit={onSubmit} className="container">
             <input name="email" type="email" placeholder="Email" value={email} onChange={onChange} className="authInput" />
             <input name="password" type="password" placeholder="Password" value={password} onChange={onChange} className="authInput" />
+            { newAccount ? <input name="passwordCheck" type="password" placeholder="Password Check" value={passwordCheck} onChange={onChange} className="authInput" /> : <></>}            
             <input type="submit" value={newAccount ? "Create Account" : "LogIn"} className="authInput authSubmit" />
             <span onClick={toggleAccount} className="authSwitch"> {newAccount? "Log IN" : "Create New Account"} </span>
             {errorMsg && <span className="authError">{errorMsg}</span>}
